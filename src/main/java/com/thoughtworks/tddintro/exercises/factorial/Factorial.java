bf622f6a6dc6eaf32fa0cdd851ec5a3a443a6459
Webs <weberswords@gmail.com>
2017-07-23 08:52:27 -0700

    Test 2 passes

--- src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
@@ -3,15 +3,7 @@ package com.thoughtworks.tddintro.exercises.factorial;
 public class Factorial {
     public Integer compute(int i) {
 
-        int sum = 0;
-        for (int n = i; n > 0; n--) {
-            if (n > 1) {
-                int result = n * (n - 1);
-                sum += result;
-            } else {
-                sum = 1;
-            }
-        }
-        return sum;
+        return i;
     }
+
 }
