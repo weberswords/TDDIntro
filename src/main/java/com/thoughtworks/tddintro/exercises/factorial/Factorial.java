87dae770741b93743917dae47973f80cc4706950
Webs <weberswords@gmail.com>
2017-07-23 09:03:26 -0700

    Test shouldReturnSix passes

--- src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
@@ -6,8 +6,17 @@ public class Factorial {
         if (i == 0) {
             return 1;
         } else {
-            return i;
+            return findFactorial(i);
         }
+    }
 
+    public Integer findFactorial(Integer i) {
+        Integer sum = 0;
+        if (i<=1) {
+            sum = 1; }
+        else {
+        sum = i*findFactorial(i-1);
+        }
+        return sum;
     }
 }
